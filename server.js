74cd89d8f1cc25ccb4db4f2955893e62a785ba96
Thakur Neupane <thakur.neupane.neupane@gmail.com>
2024-08-11 20:15:57 +1000

    server .js

--- server.js
+++ server.js
@@ -28,6 +28,27 @@ app.use("/api/v1/users", userRouter);
 //Transaction
 app.use("/api/v1/transactions", transactionRouter);
 
+app.get("/", (req, res, next) => {
+  res.json({
+    status: "success",
+    message: "server is live",
+  });
+});
+
+app.use("*", (req, res, next) => {
+  const err = new Error("404 Page nto found");
+  err.statusCode = 404;
+  next(err);
+});
+
+app.use((error, req, res, next) => {
+  res.status(error.statusCode || 500);
+  res.json({
+    status: "error",
+    message: error.message,
+  });
+});
+
 app.listen(PORT, (error) => {
   error
     ? console.log(error)
